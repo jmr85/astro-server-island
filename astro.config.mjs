11ae286a4d04d936de46aa7c8cb65b140c2e5da2
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-02-04 00:18:05 -0300

    Enable Incremental Static Regeneration (ISR) for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,7 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), solidJs(), tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    isr: true,
+  }),
 });
