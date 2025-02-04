a616e87d632fb538a3ab4a4485f2077c23aa285b
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-02-04 00:30:44 -0300

    Switch deployment platform from Vercel to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from '@astrojs/vercel';
-
 import solidJs from '@astrojs/solid-js';
 
 import tailwind from '@astrojs/tailwind';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), solidJs(), tailwind()],
   output: "server",
-  adapter: vercel({
-    isr: true,
-  }),
+  adapter: netlify(),
 });
