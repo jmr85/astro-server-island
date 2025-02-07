3149a2d055f71b455ac553682b5cd9bab437339b
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-02-07 04:15:41 -0300

    Refactor server configuration to use Cloudflare adapter and simplify start script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig,  envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -7,24 +7,14 @@ import solidJs from '@astrojs/solid-js';
 
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), solidJs(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: 'standalone',
-  }),
-  env: {
-    schema: {
-      PORT: envField.number({ context: "server", default: 10000, access: "secret" })
-    }
-  },
-  server: {
-    port: import.meta.env.PORT
-  }
+  adapter: cloudflare()
   // server: {
   //   port: process.env.PORT ? +process.env.PORT : 4000
   // }
